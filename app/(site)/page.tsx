e2d261f01829daa34cb0452d9c0f7c6d619f32d5
Pratish Ninawe <pratish.1006@gmail.com>
2024-04-27 23:54:56 +0530

    feat: Added global search, atom config and layout
    Signed-off-by: Pratish Ninawe <pratish.1006@gmail.com>

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Home (): JSX.Element {
   return (
-    <React.Fragment>
-      Hello
-    </React.Fragment>
+    <React.Fragment>Client</React.Fragment>
   )
 }
